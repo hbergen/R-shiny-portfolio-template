70495fa4ee2cac1bd32dfc89417ec5109f738fb6
hbergen <heidi.a.whiting@gmail.com>
2016-05-09 01:06:13 -0700

    Apply simpler css style

--- ui.R
+++ ui.R
@@ -14,12 +14,11 @@ library(DT)
 
 # PANEL - ABOUT ME CONTENT 
 home <- tabPanel( title = "Home"
-                    , includeCSS("www/css/bootstrap.css")
                     , mainPanel( h1("About this project")
                                   , br()
                                   , HTML("<p>Hello, World!
                                      <p>
-                                     <p> This application provides a minimal working example for a Shiny app, complete with bootstrap, favicons, navbar dropdowns, and example data and plot integration.
+                                     <p> This application provides a minimal working example for a Shiny app, complete with <a href='https://bootswatch.com/' target='blank_'>bootstrap</a>, favicons, navbar dropdowns, and example data and plot integration.
                                      <p> Created by Heidi Whiting.
                                      <p><i class='fa fa-linkedin fa-1x'></i>
                                      <a href = 'https://www.linkedin.com/in/heidiwhiting' target='_blank'> View my Linkedin profile</a>." )
@@ -51,6 +50,7 @@ A_methods <- tabPanel( title = "methods"
                         )
            )
 
+
 # NAVBAR 1 - PROJECT A RESULTS
 A_results <- tabPanel( title = "results"
                        , mainPanel(
@@ -66,14 +66,14 @@ A_results <- tabPanel( title = "results"
 # NAVBAR 2 - PROJECT B
 # Store pages in separate markdown documents to keep folders tidy
 B_background <- tabPanel( title = "Project B background"
-                          , mainPanel( includeMarkdown("B_background.Rmd" ) )
+                          , mainPanel( includeMarkdown("static/B_background.Rmd" ) )
 )
-
 
 
 resources <- tabPanel( title = "resources"
                        , mainPanel("Page for references, articles, list of packages used, ...")
                       )
+
 
 # PUT EVERYTHING TOGETHER
 shinyUI(  
@@ -87,7 +87,8 @@ shinyUI(
                          , navbarMenu( title= "project B"
                                       , B_background)
                          , resources
-                      #   , theme = "www/css/bootstrap.css" #modified flatly theme
+                       # css from https://bootswatch.com/
+                         , includeCSS("www/css/bootstrap.css")
                          , windowTitle = "A demo project"
           )
        )
